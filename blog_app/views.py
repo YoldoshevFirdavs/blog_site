4f4e44f1d47a48e87bb7f6b9bbb1c869f05db393
Firdavs <firdavsyoldoshevpython@gmail.com>
2026-03-20 17:26:14 +0500

    Initial project

--- blog_app/views.py
+++ blog_app/views.py
@@ -13,3 +13,16 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post, slug):
     post = get_object_or_404(Post, slug=slug, status='published', publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
+
+
+
+
+
+
+
+
+
+
+
+
+
